808e39789c36ade3272228462fb9b4e7e9aeec0e
deep9685 <deepaknmdv14@gmail.com>
2024-06-24 16:01:59 +0530

    Initial changes

--- socket.js
+++ socket.js
@@ -1,24 +1,24 @@
 // socket.js
 
-const socketio = require('socket.io');
+// const socketio = require('socket.io');
 
-const users = {};
+// const users = {};
 
-module.exports = (server) => {
-  const io = socketio(server);
+// module.exports = (server) => {
+//   const io = socketio(server);
 
-  io.on('connection', (socket) => {
-    socket.on('join', (data) => {
-      users[socket.id] = data.userId;
-      socket.join(data.userId);
-    });
+//   io.on('connection', (socket) => {
+//     socket.on('join', (data) => {
+//       users[socket.id] = data.userId;
+//       socket.join(data.userId);
+//     });
 
-    socket.on('message', (data) => {
-      io.to(data.to).emit('message', { from: users[socket.id], message: data.message });
-    });
+//     socket.on('message', (data) => {
+//       io.to(data.to).emit('message', { from: users[socket.id], message: data.message });
+//     });
 
-    socket.on('disconnect', () => {
-      delete users[socket.id];
-    });
-  });
-};
+//     socket.on('disconnect', () => {
+//       delete users[socket.id];
+//     });
+//   });
+// };
